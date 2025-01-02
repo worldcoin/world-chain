6006adcd07c9a57e461f897eea77d564e929abe7
Dzejkop <jakubtrad@gmail.com>
2025-01-02 16:37:31 +0100

    recognition

--- world-chain-builder/crates/world/pool/src/eip4337.rs
+++ world-chain-builder/crates/world/pool/src/eip4337.rs
@@ -1,8 +1,8 @@
 use alloy_primitives::keccak256;
-use alloy_sol_types::SolValue;
+use alloy_sol_types::{SolCall, SolValue};
 use semaphore::{hash_to_field, Field};
 
-use crate::bindings::IEntryPoint::PackedUserOperation;
+use crate::bindings::{IEntryPoint::PackedUserOperation, IPBHValidator};
 
 pub fn hash_user_op(user_op: &PackedUserOperation) -> Field {
     let keccak_hash = keccak256(SolValue::abi_encode_packed(&(
@@ -12,4 +12,12 @@ pub fn hash_user_op(user_op: &PackedUserOperation) -> Field {
     )));
 
     hash_to_field(keccak_hash.as_slice())
+}
+
+pub fn is_handle_aggregated_ops_call(calldata: &[u8]) -> bool {
+    calldata.starts_with(&IPBHValidator::handleAggregatedOpsCall::SELECTOR as &[u8])
+}
+
+pub fn is_pbh_multicall(calldata: &[u8]) -> bool {
+    calldata.starts_with(&IPBHValidator::pbhMulticallCall::SELECTOR as &[u8])
 }
