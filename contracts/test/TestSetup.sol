e9512c99679e51966ca36676c39529948f3473c8
Dzejkop <jakubtrad@gmail.com>
2025-01-07 15:52:43 +0100

    remove console import

--- contracts/test/TestSetup.sol
+++ contracts/test/TestSetup.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import {console} from "forge-std/console.sol";
 import "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {MockWorldIDGroups} from "./mocks/MockWorldIDGroups.sol";
 import {IPBHEntryPoint} from "../src/interfaces/IPBHEntryPoint.sol";
