70b1971be7d22195a8ff09e840d45391ecfcb1f4
0xKitsune <0xkitsune@protonmail.com>
2025-01-23 17:42:39 -0500

    remove unused err conversion

--- world-chain-builder/crates/world/pool/src/error.rs
+++ world-chain-builder/crates/world/pool/src/error.rs
@@ -1,6 +1,4 @@
-use reth::transaction_pool::error::{InvalidPoolTransactionError, PoolTransactionError};
-use reth::transaction_pool::{PoolTransaction, TransactionValidationOutcome};
-use reth_db::{DatabaseError, DatabaseWriteOperation};
+use reth_db::DatabaseError;
 use reth_provider::ProviderError;
 use world_chain_builder_pbh::external_nullifier::ExternalNullifierError;
 
@@ -29,65 +27,3 @@ pub enum WorldChainTransactionPoolError {
     #[error(transparent)]
     RootProvider(#[from] ProviderError),
 }
-
-//TODO: double check this? We prob shouldnt be penalizing peers for invalid semaphore proof etc
-// as not all nodes will be checking pbh constraints
-impl PoolTransactionError for WorldChainTransactionPoolInvalid {
-    fn is_bad_transaction(&self) -> bool {
-        true
-    }
-}
-
-impl From<WorldChainTransactionPoolInvalid> for Box<dyn PoolTransactionError> {
-    fn from(e: WorldChainTransactionPoolInvalid) -> Self {
-        Box::new(e)
-    }
-}
-
-// /// A Result type returned after checking a transaction's validity.
-// #[derive(Debug)]
-// pub enum TransactionValidationError {
-//     /// The transaction is considered invalid indefinitely: It violates constraints that prevent
-//     /// this transaction from ever becoming valid.
-//     Invalid(InvalidPoolTransactionError),
-//     /// An error occurred while trying to validate the transaction
-//     Error(Box<dyn std::error::Error + Send + Sync>),
-// }
-
-// impl From<WorldChainTransactionPoolInvalid> for TransactionValidationError {
-//     fn from(e: WorldChainTransactionPoolInvalid) -> Self {
-//         TransactionValidationError::Invalid(InvalidPoolTransactionError::Other(e.into()))
-//     }
-// }
-
-// impl From<WorldChainTransactionPoolError> for TransactionValidationError {
-//     fn from(e: WorldChainTransactionPoolError) -> Self {
-//         TransactionValidationError::Error(Box::new(e))
-//     }
-// }
-
-// impl From<DatabaseError> for TransactionValidationError {
-//     fn from(e: DatabaseError) -> Self {
-//         match e {
-//             DatabaseError::Write(write) => {
-//                 if let DatabaseWriteOperation::CursorInsert = write.operation {
-//                     WorldChainTransactionPoolInvalid::DuplicateTxHash.into()
-//                 } else {
-//                     WorldChainTransactionPoolError::Database(DatabaseError::Write(write)).into()
-//                 }
-//             }
-//             e => WorldChainTransactionPoolError::Database(e).into(),
-//         }
-//     }
-// }
-
-// impl TransactionValidationError {
-//     pub fn to_outcome<T: PoolTransaction>(self, tx: T) -> TransactionValidationOutcome<T> {
-//         match self {
-//             TransactionValidationError::Invalid(e) => TransactionValidationOutcome::Invalid(tx, e),
-//             TransactionValidationError::Error(e) => {
-//                 TransactionValidationOutcome::Error(*tx.hash(), e)
-//             }
-//         }
-//     }
-// }
