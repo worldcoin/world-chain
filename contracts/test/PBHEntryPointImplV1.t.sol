e30c042bc85807a16f407958bfd927220bdbc106
0xOsiris <djosiris@proton.me>
2025-02-14 11:05:49 -0700

    test: patch tests

--- contracts/test/PBHEntryPointImplV1.t.sol
+++ contracts/test/PBHEntryPointImplV1.t.sol
@@ -51,7 +51,9 @@ contract PBHEntryPointImplV1Test is TestSetup {
 
         bytes memory testCallData = hex"c0ffee";
         uint256 signalHash = abi.encodePacked(sender, pbhNonce, testCallData).hashToField();
-        vm.expectRevert(abi.encodeWithSelector(PBHEntryPointImplV1.InvalidNullifier.selector, signalHash));
+        vm.expectRevert(
+            abi.encodeWithSelector(PBHEntryPointImplV1.InvalidNullifier.selector, testPayload.nullifierHash, signalHash)
+        );
         pbhEntryPoint.verifyPbh(signalHash, testPayload);
     }
 
