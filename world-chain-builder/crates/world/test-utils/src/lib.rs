879052685de266a660d1c72283e099e7b4508af5
dancoombs <dan.coombs@alchemy.com>
2025-02-17 17:20:27 -0600

    fix: use already deployed entrypoint v0.7

--- world-chain-builder/crates/world/test-utils/src/lib.rs
+++ world-chain-builder/crates/world/test-utils/src/lib.rs
@@ -7,34 +7,34 @@ const MNEMONIC: &str = "test test test test test test test test test test test j
 
 /// Devnet Test Safes
 pub const TEST_SAFES: [Address; 6] = [
-    address!("26479c9A59462f08f663281df6098dF7e7398363"),
-    address!("CaD130298688716cDd16511C18DD88CAc327B3cA"),
-    address!("4DFE55bC8eEa517efdbb6B2d056135b350b79ca2"),
-    address!("e27eA3E8D654a0348DE1B42983F75c9594CdB2a2"),
-    address!("220e1F32E1a76093C34388D4EeEB5fc0cAc73Ffb"),
-    address!("e98F083C8C5e43593A517dA7b6b83Cb64F93c740"),
+    address!("b35520e3D456b91FBEf36D8CB83fF7847937Ad26"),
+    address!("52E5544fF7D6e1449E03Ade54a7fDc950Bb85CaB"),
+    address!("D0968D7f1B3AEEB1904615d8c65C128682D2F457"),
+    address!("48aA6040f470b8edeb23CD4B83dB1e7b45B7b530"),
+    address!("68A8A253e27b42825E3d47Eb637433CAFf1B5310"),
+    address!("58AF3b6E40603C9498fC28344DD2128a6F7FE510"),
 ];
 
 /// Devnet Test 4337 Modules
 pub const TEST_MODULES: [Address; 6] = [
-    address!("8A791620dd6260079BF849Dc5567aDC3F2FdC318"),
-    address!("A51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0"),
-    address!("0B306BF915C4d645ff596e518fAf3F9669b97016"),
-    address!("68B1D87F95878fE05B998F19b66F4baba5De1aed"),
-    address!("59b670e9fA9D0A427751Af201D676719a970857b"),
-    address!("a85233C63b9Ee964Add6F2cffe00Fd84eb32338f"),
+    address!("a513E6E4b8f2a923D98304ec87F64353C4D5C853"),
+    address!("610178dA211FEF7D417bC0e6FeD39F05609AD788"),
+    address!("0DCd1Bf9A1b36cE34237eEaFef220932846BCD82"),
+    address!("959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1"),
+    address!("3Aa5ebB10DC797CAC828524e59A333d0A371443c"),
+    address!("4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1"),
 ];
 
 pub const DEV_CHAIN_ID: u64 = 2151908;
 
 pub const PBH_NONCE_KEY: u32 = 1123123123;
 
-pub const DEVNET_ENTRYPOINT: Address = address!("9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0");
+pub const DEVNET_ENTRYPOINT: Address = address!("0000000071727De22E5E9d8BAf0edAc6f37da032");
 
 pub const PBH_DEV_SIGNATURE_AGGREGATOR: Address =
-    address!("5FC8d32690cc91D4c39d9d3abcBD16989F875707");
+    address!("Cf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9");
 
-pub const PBH_DEV_ENTRYPOINT: Address = address!("Dc64a140Aa3E981100a9becA4E685f962f0cF6C9");
+pub const PBH_DEV_ENTRYPOINT: Address = address!("9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0");
 
 pub const DEV_WORLD_ID: Address = address!("5FbDB2315678afecb367f032d93F642f64180aa3");
 
