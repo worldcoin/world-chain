06080c1409ad9d55abc2775cc8e5cee3ca92dba1
0xOsiris <djosiris@proton.me>
2025-02-27 13:18:11 -0700

    chore: update interface

--- contracts/src/interfaces/IWorldChainBlockRegistry.sol
+++ contracts/src/interfaces/IWorldChainBlockRegistry.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.28;
 interface IWorldChainBlockRegistry {
     function stampBlock() external;
     function updateBuilder(address builder) external;
+    function builtBlocks(uint256 blockNumber) external view returns (address);
 }
