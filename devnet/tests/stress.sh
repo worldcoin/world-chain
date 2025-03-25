b604e0c46e22b46f96eb5067e2c408411f408049
0xOsiris <djosiris@proton.me>
2025-03-24 22:12:16 -0700

    feat: dynamic host configuration

--- devnet/tests/stress.sh
+++ devnet/tests/stress.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -ex
+# TODO:
+# set -ex
 
-source .env
+# source .env
 
-# Download the scenario for contender
-wget https://raw.githubusercontent.com/flashbots/contender/refs/heads/main/scenarios/stress.toml -O scenario.toml
+# # Download the scenario for contender
+# wget https://raw.githubusercontent.com/flashbots/contender/refs/heads/main/scenarios/stress.toml -O scenario.toml
 
-contender setup -p $PREFUNDED_PRIV_KEY scenario.toml http://localhost:$L2_PORT
+# contender setup -p $PREFUNDED_PRIV_KEY scenario.toml http://localhost:$L2_PORT
