409a7e0c786b32720196dde88b3d40b986315ec4
Eric Woolsey <ewoolsey@ualberta.ca>
2025-08-20 16:22:53 -0700

    wip finish cleanup up

--- crates/world/flashblocks/src/rpc/eth/transaction.rs
+++ crates/world/flashblocks/src/rpc/eth/transaction.rs
@@ -14,46 +14,14 @@ where
     Rpc: RpcConvert<Primitives = N::Primitives, Error = OpEthApiError>,
 {
     fn signers(&self) -> &SignersForRpc<Self::Provider, Self::NetworkTypes> {
-        todo!()
-        // self.inner.eth_api.signers()
+        self.inner.signers()
     }
 
     /// Decodes and recovers the transaction and submits it to the pool.
     ///
     /// Returns the hash of the transaction.
     async fn send_raw_transaction(&self, tx: Bytes) -> Result<B256, Self::Error> {
-        todo!()
-        // let recovered = recover_raw_transaction(&tx)?;
-        //
-        // // broadcast raw transaction to subscribers if there is any.
-        // self.eth_api().broadcast_raw_transaction(tx.clone());
-        //
-        // let pool_transaction = <Self::Pool as TransactionPool>::Transaction::from_pooled(recovered);
-        //
-        // // On optimism, transactions are forwarded directly to the sequencer to be included in
-        // // blocks that it builds.
-        // if let Some(client) = self.raw_tx_forwarder().as_ref() {
-        //     tracing::debug!(target: "rpc::eth", hash = %pool_transaction.hash(), "forwarding raw transaction to sequencer");
-        //     let hash = client.forward_raw_transaction(&tx).await.inspect_err(|err| {
-        //             tracing::debug!(target: "rpc::eth", %err, hash=% *pool_transaction.hash(), "failed to forward raw transaction");
-        //         })?;
-        //
-        //     // Retain tx in local tx pool after forwarding, for local RPC usage.
-        //     let _ = self.inner.eth_api.add_pool_transaction(pool_transaction).await.inspect_err(|err| {
-        //         tracing::warn!(target: "rpc::eth", %err, %hash, "successfully sent tx to sequencer, but failed to persist in local tx pool");
-        //     });
-        //
-        //     return Ok(hash);
-        // }
-        //
-        // // submit the transaction to the pool with a `Local` origin
-        // let AddedTransactionOutcome { hash, .. } = self
-        //     .pool()
-        //     .add_transaction(TransactionOrigin::Local, pool_transaction)
-        //     .await
-        //     .map_err(Self::Error::from_eth_err)?;
-        //
-        // Ok(hash)
+        self.inner.send_raw_transaction(tx).await
     }
 }
 
