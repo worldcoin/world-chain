0188c609b63feaa8431f4b8297bb13635f617fcd
Eric Woolsey <ewoolsey@ualberta.ca>
2025-09-12 16:04:38 -0700

    feat: world-chain-payload

--- crates/world/node/src/node.rs
+++ crates/world/node/src/node.rs
@@ -46,7 +46,7 @@ use reth_transaction_pool::{BlobStore, TransactionPool};
 
 use crate::config::WorldChainNodeConfig;
 use tracing::{debug, info};
-use world_chain_builder_payload::builder::WorldChainPayloadBuilder;
+use world_chain_payload::builder::WorldChainPayloadBuilder;
 use world_chain_pool::ordering::WorldChainOrdering;
 use world_chain_pool::root::WorldChainRootValidator;
 use world_chain_pool::tx::{WorldChainPoolTransaction, WorldChainPooledTransaction};
