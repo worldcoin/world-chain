3755ebfe61e32ce103c15aefc83abf674922f50a
Eric Woolsey <ewoolsey@ualberta.ca>
2025-11-05 14:30:20 -0800

    Update crates/world/node/src/config.rs
    Co-authored-by: 0xOsiris <djosiris@proton.me>

--- crates/world/node/src/config.rs
+++ crates/world/node/src/config.rs
@@ -4,6 +4,7 @@ use crate::args::WorldChainArgs;
 
 #[derive(Debug, Clone)]
 pub struct WorldChainNodeConfig {
+    /// World Chain Specific CLI arguements
     pub args: WorldChainArgs,
     pub builder_config: OpBuilderConfig,
 }
