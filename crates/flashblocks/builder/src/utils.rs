30b7ed32ca78cf153bfb461db5f105816e7518fb
0xOsiris <djosiris@proton.me>
2025-12-18 16:41:43 -0800

    chore: nits

--- crates/flashblocks/builder/src/utils.rs
+++ crates/flashblocks/builder/src/utils.rs
@@ -1,5 +1,4 @@
 use std::collections::{HashMap, hash_map::Entry};
-
 use revm_database::states::reverts::{AccountInfoRevert, Reverts};
 
 /// Flattens a multi-transition [`Reverts`] into a single transition, merging per-account data.
