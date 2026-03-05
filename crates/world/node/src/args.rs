7cfae53ec0fd449c872bc949ba51439af6ba2658
Eric Woolsey <ewoolsey@ualberta.ca>
2026-03-04 18:34:44 -0800

    fix: set bootnodes when bootnodes.is_none()

--- crates/world/node/src/args.rs
+++ crates/world/node/src/args.rs
@@ -11,7 +11,7 @@ use reth_node_builder::NodeConfig;
 use reth_optimism_chainspec::OpChainSpec;
 use reth_optimism_node::args::RollupArgs;
 use std::{net::SocketAddr, path::PathBuf, str::FromStr};
-use tracing::{info, warn};
+use tracing::{debug, info, warn};
 
 pub const DEFAULT_FLASHBLOCKS_BOOTNODES: &str = "enode://78ca7daeb63956cbc3985853d5699a6404d976a2612575563f46876968fdca2383a195ee7db40de348757b2256195996933708f351169ca3f3fe93ab2a774608@16.62.98.53:30303,enode://c96dcadf4cdea4c39ec3fd775637d9e67d455b856b1514cfcf55b72f873a34b96d69e47ccea9fc797a446d4e6948aa80f6b9d479a1727ca166758a900b08f422@16.63.14.166:30303,enode://15688a7b281c32a4da633252dcc5019d60f037ee9eb46d05093dd3023bdd688b9b207d10a39e054a5ed87db666b2cb75696f6537de74d1e1f8dcabc53dc8d2ab@16.63.123.160:30303";
 
@@ -102,11 +102,10 @@ impl WorldChainArgs {
                     )?);
                 }
 
-                if let Some(bootnodes) = &mut config.network.bootnodes
-                    && bootnodes.is_empty()
-                    && self.flashblocks.is_some()
-                {
-                    *bootnodes = parse_trusted_peer(DEFAULT_FLASHBLOCKS_BOOTNODES)?;
+                if config.network.bootnodes.is_none() && self.flashblocks.is_some() {
+                    let bootnodes = parse_trusted_peer(DEFAULT_FLASHBLOCKS_BOOTNODES)?;
+                    debug!(target: "world_chain::network", ?bootnodes, "Setting default flashblocks bootnodes");
+                    config.network.bootnodes = Some(bootnodes);
                 }
 
                 if self.pbh.entrypoint == Address::default() {
