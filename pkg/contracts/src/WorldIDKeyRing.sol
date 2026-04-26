ea92e70cd41cdef500b238923e80bddfe8ed2d6c
0xOsiris <djosiris@proton.me>
2026-04-26 15:06:02 -0700

    chore: add ref-impl for wip-1001 key chain

--- pkg/contracts/src/WorldIDKeyRing.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-/// @title WorldChainKeyRing
-/// @author Worldcoin Contributors
-/// @notice A contract which manages the creation of and management of session keys for World ID KeyChains.
-contract WorldIDKeyRing {
-
-}
