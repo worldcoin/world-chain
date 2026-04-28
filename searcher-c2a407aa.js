1e3786b71310431f3a2ae80bb8cb9fab872af29a
0xForerunner <0xForerunner@users.noreply.github.com>
2026-04-28 00:01:27 +0000

    deploy: fabffc24fda77dda0b2315595612d398f6423f28

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-287d2014.js',
+                path_to_root + 'searchindex-0e060b7c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
