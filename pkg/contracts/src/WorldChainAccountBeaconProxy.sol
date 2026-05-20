72e25b8e07cada16d08008c88d181620eab4556a
0xOsiris <djosiris@proton.me>
2026-05-20 16:11:27 -0700

    chore: update specification

--- pkg/contracts/src/WorldChainAccountBeaconProxy.sol
+++ pkg/contracts/src/WorldChainAccountBeaconProxy.sol
@@ -7,8 +7,9 @@ import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol"
 /// @author 0xOsiris, World Contributors
 /// @custom:security-contact security@toolsforhumanity.com
 contract WorldChainAccountBeaconProxy is BeaconProxy {
-    /// @notice The single global `WorldChainAccountUpgradeableBeacon` predeploy.
-    /// TODO: FIXME(Osiris): Update Spec
+    /// @notice The single global `WorldChainAccountUpgradeableBeacon` predeploy. See WIP-1001
+    ///         "Beacon-Proxied Account Router" for the role of this address in the WIP-1001
+    ///         account architecture.
     address internal constant WORLD_CHAIN_ACCOUNT_BEACON = 0x000000000000000000000000000000000000B0cC;
 
     constructor() BeaconProxy(WORLD_CHAIN_ACCOUNT_BEACON, "") {}
