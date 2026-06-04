3695478f47fd9f02f3a206a2ebf07d3699e07d2d
alessandromazza98 <alessandro98.mazza@gmail.com>
2026-06-04 14:47:34 +0200

    short circuit if from >= target

--- proofs/challenger/src/challenger.rs
+++ proofs/challenger/src/challenger.rs
@@ -50,6 +50,11 @@ where
         } else {
             self.cursor
         };
+        // short circuit if from >= target: it means that there are no
+        // new L1 finalized blocks compared to last scan
+        if from >= target {
+            return Ok(());
+        }
         let game_created = self.provider.games_created(from, target).await?;
         for game_created in game_created {
             let game = game_created.game;
