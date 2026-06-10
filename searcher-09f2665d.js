b92fc333a39c14cb32b247efde32badce19c22c9
Dzejkop <Dzejkop@users.noreply.github.com>
2026-06-10 16:12:20 +0000

    deploy: 2a2636f40885758dcae917b801c2684f5afed824

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-28d2d783.js',
+                path_to_root + 'searchindex-dce329de.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
