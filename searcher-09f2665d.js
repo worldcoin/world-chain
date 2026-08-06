b6a95bbeabd17a003e771b786b3556955f375733
0xOsiris <0xOsiris@users.noreply.github.com>
2026-08-06 18:05:41 +0000

    deploy: 57fd5b5d49f12dbd3140025b62dc9ffd9636a409

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f93779d3.js',
+                path_to_root + 'searchindex-32652eb9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
